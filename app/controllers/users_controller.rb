4d09b4c120ba7a23da5c964300aa83db5b928b77
matthewkendrick <75340042+matthewkendrick@users.noreply.github.com>
2022-01-30 04:25:23 +0000

    [Finish]Application2_chapter15

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,8 @@
 class UsersController < ApplicationController
   def show
+    #  URLに記載されたIDを参考に、必要なUserモデルを取得する処理を記述する
+    @user = User.find(params[:id])
+    @post_images = @user.post_images
   end
 
   def edit
